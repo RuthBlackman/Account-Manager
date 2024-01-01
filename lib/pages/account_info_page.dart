c882dec7e1867b1fd4316b5231a16b226d796196
Ruth Blackman <rkjb410@gmail.com>
2024-01-01 17:20:41 +0000

    fixed overflow issue

--- lib/pages/account_info_page.dart
+++ lib/pages/account_info_page.dart
@@ -34,144 +34,145 @@ class AccountInfoPage extends StatelessWidget {
     return Scaffold(
       appBar: const MyAppBar(title: "Account Information"),
       backgroundColor: blueBackground,
-      body: Column(
-        children: [
-
-          // score
-          Padding(
-            padding: const EdgeInsets.only(top: 20),
-            child: Container(
-              alignment: Alignment.center, // need container within container to center align
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            // score
+            Padding(
+              padding: const EdgeInsets.only(top: 20),
               child: Container(
-                width: 80,
-                height: 80,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  color: green,
-                ),
-                child: const Align(
-                    alignment: Alignment.center,
-                    child: Text(
-                      "0",
-                      style: TextStyle(
-                        fontSize: 30,
-                      ),
-                    )
+                alignment: Alignment.center, // need container within container to center align
+                child: Container(
+                  width: 80,
+                  height: 80,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: green,
+                  ),
+                  child: const Align(
+                      alignment: Alignment.center,
+                      child: Text(
+                        "0",
+                        style: TextStyle(
+                          fontSize: 30,
+                        ),
+                      )
+                  ),
                 ),
               ),
             ),
-          ),
 
-          // custom container with account functionality
-          MyContainer(
-            isStarsContainer: false,
-            height: 500,
-            children: [
-              WidgetWithCustomWidth(widget: AccountNameWidget()),
+            // custom container with account functionality
+            MyContainer(
+              isStarsContainer: false,
+              height: 500,
+              children: [
+                WidgetWithCustomWidth(widget: AccountNameWidget()),
 
-              WidgetWithCustomWidth(
-                widget: CategoryDropdown(),
-                width: 250,
-              ),
-
-              WidgetWithCustomWidth(
+                WidgetWithCustomWidth(
+                  widget: CategoryDropdown(),
                   width: 250,
-                  widget: MyTextField(
-                    hintText: "Username",
-                    obscureText: false,
-                    controller: usernameController,
-                  )
-              ),
-
-              WidgetWithCustomWidth(
-                  width: 250,
-                  widget: MyTextField(
-                    hintText: "Password",
-                    obscureText: true,
-                    controller: passwordController,
-                  )
-              ),
-
-              WidgetWithCustomWidth(
-                widget: Padding(
-                  padding: const EdgeInsets.only(top: 30.0),
-                  child: MyButton(
-                    text: "Enter More Info",
-                    width: 100,
-                    height: 75,
-                    color: greyButton,
-                    onTap: (){
-                      _showDialog(context);
-                    },
-                  ),
                 ),
-                width: 200,
-              ),
 
-              WidgetWithCustomWidth(
-                  widget: Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(top: 20.0, right: 20.0),
-                        child: MyButton(
-                          text: "Save",
-                          width: 90,
-                          height: 90,
-                          color: greyButton,
-                          icon: Icons.add,
-                          onTap: (){},
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(top: 20.0),
-                        child: MyButton(
-                          text: "Exit",
-                          width: 90,
-                          height: 90,
-                          color: greyButton,
-                          icon: Icons.exit_to_app,
-                          onTap: (){},
-                        ),
-                      ),
-                      if(firstTimeUer)  Padding(
-                        padding: const EdgeInsets.only(top: 20.0, left: 20.0),
-                        child: MyButton(
-                          text: "Next",
-                          width: 90,
-                          height: 90,
-                          color: greyButton,
-                          icon: Icons.arrow_forward_outlined,
-                          onTap: (){},
-                        ),
-                      ),
-                    ],
-                  )
-              ),
-            ],
-          ),
+                WidgetWithCustomWidth(
+                    width: 250,
+                    widget: MyTextField(
+                      hintText: "Username",
+                      obscureText: false,
+                      controller: usernameController,
+                    )
+                ),
 
-          // stars
-          MyContainer(
-            height: 75,
-            isStarsContainer: true,
-            children: [
-              WidgetWithCustomWidth(
-                  widget: const Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      StarIcon(icon: Icons.star),
-                      StarIcon(icon: Icons.star),
-                      StarIcon(icon: Icons.star),
-                      StarIcon(icon: Icons.star),
-                    ],
-                  )
-              )
-            ],
-          )
-        ],
+                WidgetWithCustomWidth(
+                    width: 250,
+                    widget: MyTextField(
+                      hintText: "Password",
+                      obscureText: true,
+                      controller: passwordController,
+                    )
+                ),
+
+                WidgetWithCustomWidth(
+                  widget: Padding(
+                    padding: const EdgeInsets.only(top: 30.0),
+                    child: MyButton(
+                      text: "Enter More Info",
+                      width: 100,
+                      height: 75,
+                      color: greyButton,
+                      onTap: (){
+                        _showDialog(context);
+                      },
+                    ),
+                  ),
+                  width: 200,
+                ),
+
+                WidgetWithCustomWidth(
+                    widget: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(top: 20.0, right: 20.0),
+                          child: MyButton(
+                            text: "Save",
+                            width: 90,
+                            height: 90,
+                            color: greyButton,
+                            icon: Icons.add,
+                            onTap: (){},
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 20.0),
+                          child: MyButton(
+                            text: "Exit",
+                            width: 90,
+                            height: 90,
+                            color: greyButton,
+                            icon: Icons.exit_to_app,
+                            onTap: (){},
+                          ),
+                        ),
+                        if(firstTimeUer)  Padding(
+                          padding: const EdgeInsets.only(top: 20.0, left: 20.0),
+                          child: MyButton(
+                            text: "Next",
+                            width: 90,
+                            height: 90,
+                            color: greyButton,
+                            icon: Icons.arrow_forward_outlined,
+                            onTap: (){},
+                          ),
+                        ),
+                      ],
+                    )
+                ),
+              ],
+            ),
+
+            // stars
+            MyContainer(
+              height: 75,
+              isStarsContainer: true,
+              children: [
+                WidgetWithCustomWidth(
+                    widget: const Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        StarIcon(icon: Icons.star),
+                        StarIcon(icon: Icons.star),
+                        StarIcon(icon: Icons.star),
+                        StarIcon(icon: Icons.star),
+                      ],
+                    )
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
